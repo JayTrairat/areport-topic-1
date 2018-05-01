dab907465126bf14edc9563f5c29e5189e1c16d4
JayTrairat <JayTrairat@JayTrairats-MacBook-Pro.local>
2018-05-01 15:39:43 +0700

    refactor(): yml and combinator

--- main.py
+++ main.py
@@ -5,8 +5,22 @@ import yaml
 def main():
     with open('assets/important_words/pos_1.yml', 'r', encoding='utf8') as source:
         PRE = yaml.load(source)
+    with open('assets/important_words/pos_2.yml', 'r', encoding='utf8') as source:
+        MIDPRE = yaml.load(source)
+    with open('assets/important_words/pos_3.yml', 'r', encoding='utf8') as source:
+        MIDPOST = yaml.load(source)
+    with open('assets/important_words/pos_4.yml', 'r', encoding='utf8') as source:
+        POST = yaml.load(source)
+    type = "number"
+    naming_list = [
+        pre + midpre + midpost + post
+        for pre in PRE['all']
+        for midpre in MIDPRE['all']
+        for midpost in MIDPOST['all']
+        for post in POST[type]
+    ]
 
-    print(PRE)
+    print(naming_list)
 
 
 if __name__ == '__main__':
